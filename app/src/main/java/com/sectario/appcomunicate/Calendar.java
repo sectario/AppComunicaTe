56bdd4998e9a79f5c5e88937f93a8baadabc4a99
desinnio <desinnio@gmail.com>
2022-11-15 20:40:36 -0500

    Creación activity Calendar

--- app/src/main/java/com/sectario/appcomunicate/Calendar.java
+++ app/src/main/java/com/sectario/appcomunicate/Calendar.java
@@ -3,12 +3,15 @@ package com.sectario.appcomunicate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class Calendar extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
     }
 }
