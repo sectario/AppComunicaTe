69be6eef74300de3bc4288db8008625503ad03cc
Jennifer M <ing.jennifermartinez@gmail.com>
2022-11-15 21:42:43 -0500

    Primer commit Jennifer V2

--- app/src/main/java/com/sectario/appcomunicate/Login.java
+++ app/src/main/java/com/sectario/appcomunicate/Login.java
@@ -2,13 +2,33 @@ package com.sectario.appcomunicate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button btnRegistrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnRegistrarse=findViewById(R.id.btnRegistrarse);
+        Intent i = new Intent(this, Register.class);
+
+        btnRegistrarse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(i);
+
+            }
+        });
+
+
+
     }
 }
